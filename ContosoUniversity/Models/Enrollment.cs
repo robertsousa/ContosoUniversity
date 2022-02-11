f3a0064325ea1ca81b2d158f70b3ec1e74e0a069
Roberto Sousa <robert6257@gmail.com>
2022-02-11 09:08:07 -0300

    Create the database context

--- ContosoUniversity/Models/Enrollment.cs
+++ ContosoUniversity/Models/Enrollment.cs
@@ -5,7 +5,19 @@ using System.Threading.Tasks;
 
 namespace ContosoUniversity.Models
 {
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+
     public class Enrollment
     {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+        public Grade? Grade { get; set; }
+
+        public Course Course { get; set; }
+        public Student Student { get; set; }
     }
 }
