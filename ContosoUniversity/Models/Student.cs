c674ab373eabb23379586ae60d39980da4aa2945
Roberto Sousa <robert6257@gmail.com>
2022-02-12 15:17:06 -0300

    The StringLength attribute

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -9,7 +9,9 @@ namespace ContosoUniversity.Models
     public class Student
     {
         public int ID { get; set; }
+        [StringLength(50)]
         public string LastName { get; set; }
+        [StringLength(50)]
         public string FirstMidName { get; set; }
 
         [DataType(DataType.Date)]
